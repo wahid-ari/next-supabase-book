0869175d7bf3b5eb6016073973160918ca8ade1e
wahid-ari <wahiid.arii@gmail.com>
2023-05-28 18:15:55 +0700

    create swr for author

--- libs/swr.ts
+++ libs/swr.ts
@@ -4,6 +4,16 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const API_URL = `${process.env.NEXT_PUBLIC_API_ROUTE}/api`;
 
+export function useAuthorsData() {
+  const { data, error, isLoading } = useSWR(`${API_URL}/author`, fetcher);
+  return { data, error, isLoading };
+}
+
+export function useAuthorData(id: string) {
+  const { data, error, isLoading } = useSWR(id ? `${API_URL}/author?id=${id}` : `${API_URL}/author`, fetcher);
+  return { data, error, isLoading };
+}
+
 export function useTagsData() {
   const { data, error, isLoading } = useSWR(`${API_URL}/tag`, fetcher);
   return { data, error, isLoading };
