d066159c16a24d19d92071d4317aa328eaf956b1
wahid-ari <wahiid.arii@gmail.com>
2023-05-30 16:21:29 +0700

    update quote endpoint add author when GET all quote

--- pages/api/quote/index.ts
+++ pages/api/quote/index.ts
@@ -7,7 +7,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   switch (method) {
     case 'GET':
       if (!query.id) {
-        const { data } = await supabase.from('book_quotes').select(`*`).order('id');
+        const { data } = await supabase.from('book_quotes').select(`*, book_authors (id, name)`).order('id');
         res.status(200).json(data);
       } else {
         const { data: tags } = await supabase.from('book_tags').select(`*`).order('id');
