d6398a8b2d3c4c1c9c26046ece540722932ce201
wahid-ari <wahiid.arii@gmail.com>
2023-05-31 21:01:55 +0700

    update quote endpoint add cache when get all data

--- pages/api/quote/index.ts
+++ pages/api/quote/index.ts
@@ -10,6 +10,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     case 'GET':
       if (!query.id) {
         const { data } = await supabase.from('book_quotes').select(`*, book_authors (id, name)`).order('id');
+        // https://nextjs.org/docs/api-reference/next.config.js/headers#cache-control
+        res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59');
         res.status(200).json(data);
       } else {
         const { data: tags } = await supabase.from('book_tags').select(`*`).order('id');
