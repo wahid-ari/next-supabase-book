aa2adef91501e37f199a23161922a844c90ba594
wahid-ari <wahiid.arii@gmail.com>
2023-05-31 21:02:45 +0700

    update endpoint book get only seo data

--- pages/api/book/index.ts
+++ pages/api/book/index.ts
@@ -8,6 +8,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     case 'GET':
       if (!query.id) {
         const { data } = await supabase.from('book_books').select(`*`).order('id');
+        // https://nextjs.org/docs/api-reference/next.config.js/headers#cache-control
+        res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59');
+        res.status(200).json(data);
+      } else if (query.id && query.seo) {
+        const { data } = await supabase.from('book_books').select(`title, description`).eq('id', query.id).single();
+        // https://nextjs.org/docs/api-reference/next.config.js/headers#cache-control
+        res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59');
         res.status(200).json(data);
       } else {
         const { data: genres } = await supabase.from('book_genres').select(`*`).order('id');
