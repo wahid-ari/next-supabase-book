fbfb0cc3410ee9c9ee9764e4e77d6e1b90c34671
wahid-ari <wahiid.arii@gmail.com>
2023-06-01 17:25:50 +0700

    update detail tag page

--- pages/tag/detail/[id].tsx
+++ pages/tag/detail/[id].tsx
@@ -48,7 +48,7 @@ export default function Tag({ id }) {
         Cell: (row: any) => {
           const { values, original } = row.cell.row;
           let length = values.quote.length;
-          let text = length > 50 ? values.quote.slice(0, 65) + '...' : values.quote;
+          let text = length > 50 ? values.quote.slice(0, 65) + ' ...' : values.quote;
           return (
             <HoverCard.Root>
               <HoverCard.Trigger>
