9f962807e1e920adde45e124c0492c9ab9443e51
wahid-ari <wahiid.arii@gmail.com>
2023-06-01 15:04:04 +0700

    update Dashboard page

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,10 +14,10 @@ function Card({ title, link, count, icon }: { title: string; link: string; count
       className='group flex items-center justify-between gap-2 rounded-md border p-4 shadow dark:border-neutral-800'
     >
       <div>
-        <p className='mb-2 !text-lg font-semibold text-neutral-600 transition-all group-hover:text-emerald-500 dark:text-neutral-300'>
+        <p className='mb-1 text-xl font-extrabold text-neutral-800 dark:text-neutral-100'>{count}</p>
+        <p className='text-base font-semibold text-neutral-600 transition-all group-hover:text-emerald-500 dark:text-neutral-300'>
           {title}
         </p>
-        <p className='!text-xl font-extrabold text-neutral-600 dark:text-neutral-200'>{count}</p>
       </div>
       <div className='h-12 w-12 text-neutral-300 transition-all duration-500 group-hover:text-emerald-500 dark:text-neutral-700 dark:group-hover:text-emerald-600'>
         {icon}
