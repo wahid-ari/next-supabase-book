067bc47f402b05e88a2cc672da9ef2a48f1d2af9
wahid-ari <wahiid.arii@gmail.com>
2023-06-02 16:29:53 +0700

    move Chart from statistic to Dashboard page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,46 @@
-import Layout from '@components/layout/Layout';
-import Title from '@components/systems/Title';
-import Shimer from '@components/systems/Shimer';
-import Heading from '@components/systems/Heading';
-import { useCountsData } from '@libs/swr';
+import { ReactNode, useState, useEffect } from 'react';
 import Link from 'next/link';
+import { useTheme } from 'next-themes';
+import {
+  useBookByAuthorData,
+  useBookByGenreData,
+  useQuoteByAuthorData,
+  useQuoteByTagData,
+  useCountsData,
+} from '@libs/swr';
+import Layout from '@components/layout/Layout';
+import Titles from '@components/systems/Title';
+import Shimer from '@components/systems/Shimer';
+import Text from '@components/systems/Text';
 import { AnnotationIcon, BookOpenIcon, CollectionIcon, ColorSwatchIcon, UserGroupIcon } from '@heroicons/react/outline';
-import { ReactNode } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Title,
+  ArcElement,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Bar, Doughnut, Pie } from 'react-chartjs-2';
+import { populateData, options, optionsBarChart, optionsHorizontalBarChart } from '@utils/chartSetup';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Title,
+  ArcElement,
+  Tooltip,
+  Filler,
+  Legend
+);
 
 function Card({ title, link, count, icon }: { title: string; link: string; count: number; icon?: ReactNode }) {
   return (
@@ -28,8 +63,30 @@ function Card({ title, link, count, icon }: { title: string; link: string; count
 
 export default function Home() {
   const { data, error } = useCountsData();
+  const { theme } = useTheme();
+  const { data: bookByGenre, error: errorBookByGenre } = useBookByGenreData();
+  const { data: bookByAuthor, error: errorBookByAuthor } = useBookByAuthorData();
+  const { data: quoteByAuthor, error: errorQuoteByAuthor } = useQuoteByAuthorData();
+  const { data: quoteByTag, error: errorQuoteByTag } = useQuoteByTagData();
 
-  if (error) {
+  const [dataBookByGenre, setDataBookByGenre] = useState(null);
+  const [dataBookByAuthor, setDataBookByAuthor] = useState(null);
+  const [dataQuoteByAuthor, setDataQuoteByAuthor] = useState(null);
+  const [dataQuoteByTag, setDataQuoteByTag] = useState(null);
+
+  const [windowWidth, setWindowWidth] = useState(0);
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+  }, [windowWidth]);
+
+  useEffect(() => {
+    if (bookByGenre !== undefined) setDataBookByGenre(populateData(bookByGenre, 'book'));
+    if (bookByAuthor !== undefined) setDataBookByAuthor(populateData(bookByAuthor, 'book'));
+    if (quoteByAuthor !== undefined) setDataQuoteByAuthor(populateData(quoteByAuthor, 'quote'));
+    if (quoteByTag !== undefined) setDataQuoteByTag(populateData(quoteByTag, 'quote'));
+  }, [bookByGenre, bookByAuthor, quoteByAuthor, quoteByTag]);
+
+  if (error || errorBookByAuthor || errorBookByGenre || errorQuoteByAuthor || errorQuoteByTag) {
     return (
       <Layout title='Dashboard - MyBook'>
         <div className='flex h-[36rem] items-center justify-center text-base'>Failed to load</div>
@@ -39,7 +96,7 @@ export default function Home() {
 
   return (
     <Layout title='Dashboard - MyBook'>
-      <Title>Dashboard</Title>
+      <Titles>Dashboard</Titles>
 
       <div className='mt-8 grid grid-cols-1 gap-4 min-[350px]:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'>
         {data ? (
@@ -58,6 +115,69 @@ export default function Home() {
             <Shimer className='!h-24 w-full' />
             <Shimer className='!h-24 w-full' />
           </>
+        )}
+      </div>
+
+      <div className='mt-5 grid grid-cols-1 gap-5 md:grid-cols-2'>
+        {dataBookByGenre ? (
+          <div className='rounded-md border bg-white dark:border-neutral-800 dark:bg-neutral-900'>
+            <div className='bg-neutral-100/80 p-3 dark:bg-[#1F1F1F]'>
+              <Text.medium className='!text-sm'>Total Book by Genre</Text.medium>
+            </div>
+            <div className='m-auto w-72 py-3'>
+              <Pie options={options} data={dataBookByGenre} />
+            </div>
+          </div>
+        ) : (
+          <Shimer className='!h-[350px] w-full' />
+        )}
+
+        {dataQuoteByTag ? (
+          <div className='rounded-md border bg-white dark:border-neutral-800 dark:bg-neutral-900'>
+            <div className='bg-neutral-100/80 p-3 dark:bg-[#1F1F1F]'>
+              <Text.medium className='!text-sm'>Total Quote by Tag</Text.medium>
+            </div>
+            <div className='m-auto w-72 py-3'>
+              <Doughnut options={options} data={dataQuoteByTag} />
+            </div>
+          </div>
+        ) : (
+          <Shimer className='!h-[350px] w-full' />
+        )}
+      </div>
+
+      <div className='mt-5'>
+        {dataBookByAuthor ? (
+          <div className='rounded-md border bg-white dark:border-neutral-800 dark:bg-neutral-900'>
+            <div className='bg-neutral-100/80 p-3 dark:bg-[#1F1F1F]'>
+              <Text.medium className='!text-sm'>Total Book by Author</Text.medium>
+            </div>
+            <div className='p-3'>
+              <Bar options={optionsBarChart(theme)} data={dataBookByAuthor} height={windowWidth > 500 ? 100 : 250} />
+            </div>
+          </div>
+        ) : (
+          <Shimer className='!h-96 w-full' />
+        )}
+      </div>
+
+      <div className='mt-5'>
+        {dataQuoteByAuthor ? (
+          <div className='rounded-md border bg-white dark:border-neutral-800 dark:bg-neutral-900'>
+            <div className='bg-neutral-100/80 p-3 dark:bg-[#1F1F1F]'>
+              <Text.medium className='!text-sm'>Total Quote by Author</Text.medium>
+            </div>
+            <div className='p-3'>
+              <Bar
+                // @ts-ignore
+                options={optionsHorizontalBarChart(theme, windowWidth)}
+                data={dataQuoteByAuthor}
+                height={windowWidth > 500 ? 100 : 250}
+              />
+            </div>
+          </div>
+        ) : (
+          <Shimer className='!h-96 w-full' />
         )}
       </div>
     </Layout>
