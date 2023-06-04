16273ee8e68c81421367e7e73cf7077a054238a8
wahid-ari <wahiid.arii@gmail.com>
2023-06-04 15:28:07 +0700

    add ShowMore component

---
+++ components/systems/ShowMore.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { ChevronDownIcon } from '@heroicons/react/outline';
+import { clsx } from 'clsx';
+
+type Props = {
+  className?: string;
+  children: string;
+  count?: number;
+};
+
+export default function ShowMore({ className, children, count = 200 }: Props) {
+  const [showMore, setShowMore] = useState(false);
+  const text = showMore ? children : children?.slice(0, count) + '...';
+
+  return (
+    <div className={`${className ? className + ' ' : ''} relative`}>
+      <p>{text}</p>
+      <div className='relative py-3' role='seperator'>
+        <div
+          className={`absolute bottom-3 z-0 h-8 w-full bg-gradient-to-b from-white/70 to-white dark:from-neutral-900/70 dark:to-neutral-900 ${
+            showMore && 'hidden'
+          }`}
+        />
+        <div className='relative z-[1] border-b border-neutral-200 dark:border-neutral-700' />
+        <button
+          onClick={() => setShowMore(!showMore)}
+          className={clsx(
+            'group absolute left-1/2 top-1/2 z-[2] flex -translate-x-1/2 -translate-y-1/2 transform',
+            'items-center gap-x-1 whitespace-nowrap rounded-full border bg-white px-2.5 py-0.5',
+            'shadow transition-all dark:border-neutral-700 dark:bg-neutral-900',
+            'text-xs text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300'
+          )}
+        >
+          {showMore ? 'Show Less' : 'Show More'}
+          <ChevronDownIcon
+            className={`h-4 w-4 text-gray-500 group-hover:text-gray-700 dark:text-gray-400 dark:group-hover:text-gray-300 ${
+              showMore ? 'rotate-180 transition-all duration-500' : 'rotate-0 transition-all duration-300'
+            }`}
+          />
+        </button>
+      </div>
+    </div>
+  );
+}
