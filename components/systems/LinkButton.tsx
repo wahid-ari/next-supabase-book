9785e1ba1e81246a8f798548b495886fb547cdf0
wahid-ari <wahiid.arii@gmail.com>
2023-06-04 15:27:33 +0700

    update LinkButton component

--- components/systems/LinkButton.tsx
+++ components/systems/LinkButton.tsx
@@ -29,7 +29,7 @@ LinkButton.secondary = ({ className, href, children, ...props }: Props) => {
       {...props}
       className={`${
         className ? className + ' ' : ''
-      }text-sm rounded border border-neutral-300 bg-gray-50 px-3 py-1.5 font-medium text-neutral-800 outline-none transition-all hover:bg-gray-100 dark:border-neutral-800 dark:bg-neutral-800 dark:text-neutral-300 dark:hover:bg-neutral-900`}
+      }text-sm rounded border border-neutral-300 bg-gray-50 px-3 py-1.5 font-medium text-neutral-800 outline-none transition-all hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-emerald-400 dark:border-neutral-800 dark:bg-neutral-800 dark:text-neutral-300 dark:hover:bg-neutral-900`}
     >
       {children}
     </Link>
@@ -43,7 +43,7 @@ LinkButton.tertary = ({ className, href, children, ...props }: Props) => {
       {...props}
       className={`${
         className ? className + ' ' : ''
-      }text-sm rounded px-3 py-1.5 font-medium text-neutral-600 outline-none transition-all hover:bg-neutral-100 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:hover:text-neutral-200`}
+      }text-sm rounded px-3 py-1.5 font-medium text-neutral-600 outline-none transition-all hover:bg-neutral-100 focus:outline-none focus:ring-2 focus:ring-emerald-400 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:hover:text-neutral-200`}
     >
       {children}
     </Link>
