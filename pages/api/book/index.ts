afd782909571249f00ce6729a2192481cc63753f
wahid-ari <wahiid.arii@gmail.com>
2023-06-08 19:56:36 +0700

    update book api endpoint, secure create, update, delete

--- pages/api/book/index.ts
+++ pages/api/book/index.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { supabase } from '@libs/supabase';
+import { supabase, getSessionToken, writeLogs } from '@libs/supabase';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method, body, query } = req;
@@ -50,20 +50,84 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       break;
 
     case 'POST':
-      if (!body.title) {
-        res.status(422).json({ error: 'Title required' });
-      } else {
-        // get genre string from array
-        let genre_string = ',';
-        body.genre?.forEach((item: any) => {
-          genre_string = genre_string + ', ' + item.label;
-        });
-        let clean_genre_string = genre_string.replace(',,', '').replace(' ', '');
-        // insert book
-        const { data, error } = await supabase
-          .from('book_books')
-          .insert([
-            {
+      // Check session
+      const sessionPost = await getSessionToken(res, header, token);
+      if (sessionPost) {
+        if (!body.title) {
+          res.status(422).json({ error: 'Title required' });
+        } else {
+          // get genre string from array
+          let genre_string = ',';
+          body.genre?.forEach((item: any) => {
+            genre_string = genre_string + ', ' + item.label;
+          });
+          let clean_genre_string = genre_string.replace(',,', '').replace(' ', '');
+          // insert book
+          const { data, error } = await supabase
+            .from('book_books')
+            .insert([
+              {
+                author_id: body.author_id,
+                title: body.title,
+                isbn: body.isbn,
+                language: body.language,
+                pages: body.pages,
+                published: body.published ? body.published : null,
+                link: body.link,
+                image: body.image,
+                description: body.description,
+                genre: clean_genre_string == ',' ? '' : clean_genre_string,
+              },
+            ])
+            .select();
+          if (error) {
+            res.status(422).json({ error: error.message });
+          }
+          // get book id after inserting
+          const bookId = data[0].id;
+          // if new book have genre
+          if (body.genre?.length > 0) {
+            // create array of genre of a book
+            let genre = [];
+            body.genre.forEach((item: any) => {
+              genre.push({
+                book_id: bookId,
+                genre_id: item.value,
+              });
+            });
+            // insert genre of a book to book_books_genres table
+            const { error } = await supabase.from('book_books_genres').insert(genre);
+            if (error) {
+              res.status(422).json({ error: error.message });
+            }
+          }
+          // Write logs
+          const errorLogs = await writeLogs(sessionPost.user_id, 'create', 'book_books');
+          if (errorLogs) {
+            res.status(422).json({ error: error.message });
+          }
+          res.status(200).json({ message: 'Success add book' });
+        }
+      }
+      break;
+
+    case 'PUT':
+      // Check session
+      const sessionPut = await getSessionToken(res, header, token);
+      if (sessionPut) {
+        if (!body.title) {
+          res.status(422).json({ error: 'Title required' });
+        } else {
+          // get genre string from array
+          let genre_string = ',';
+          body.genre.forEach((item: any) => {
+            genre_string = genre_string + ', ' + item.label;
+          });
+          let clean_genre_string = genre_string.replace(',,', '').replace(' ', '');
+          // update book
+          const { error } = await supabase
+            .from('book_books')
+            .update({
               author_id: body.author_id,
               title: body.title,
               isbn: body.isbn,
@@ -73,93 +137,47 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
               link: body.link,
               image: body.image,
               description: body.description,
-              genre: clean_genre_string == ',' ? '' : clean_genre_string,
-            },
-          ])
-          .select();
-        if (error) {
-          res.status(422).json({ error: error.message });
-        }
-        // get book id after inserting
-        const bookId = data[0].id;
-        // if new book have genre
-        if (body.genre?.length > 0) {
-          // create array of genre of a book
-          let genre = [];
-          body.genre.forEach((item: any) => {
-            genre.push({
-              book_id: bookId,
-              genre_id: item.value,
-            });
-          });
-          // insert genre of a book to book_books_genres table
-          const { error } = await supabase.from('book_books_genres').insert(genre);
+              genre: clean_genre_string,
+            })
+            .eq('id', body.id);
           if (error) {
             res.status(422).json({ error: error.message });
           }
-        }
-        res.status(200).json({ message: 'Success add book' });
-      }
-      break;
-
-    case 'PUT':
-      if (!body.title) {
-        res.status(422).json({ error: 'Title required' });
-      } else {
-        // get genre string from array
-        let genre_string = ',';
-        body.genre.forEach((item: any) => {
-          genre_string = genre_string + ', ' + item.label;
-        });
-        let clean_genre_string = genre_string.replace(',,', '').replace(' ', '');
-        // update book
-        const { error } = await supabase
-          .from('book_books')
-          .update({
-            author_id: body.author_id,
-            title: body.title,
-            isbn: body.isbn,
-            language: body.language,
-            pages: body.pages,
-            published: body.published ? body.published : null,
-            link: body.link,
-            image: body.image,
-            description: body.description,
-            genre: clean_genre_string,
-          })
-          .eq('id', body.id);
-        if (error) {
-          res.status(422).json({ error: error.message });
-        }
-        // delete genre related to edited book
-        const { error: errorBooksGenres } = await supabase.from('book_books_genres').delete().eq('book_id', body.id);
-        if (errorBooksGenres) {
-          res.status(422).json({ error: errorBooksGenres.message });
-        }
-        // if edited book have genre
-        if (body.genre?.length > 0) {
-          // create array of genre of a edited book
-          let genre = [];
-          body.genre.forEach((item: any) => {
-            genre.push({
-              book_id: body.id,
-              genre_id: item.value,
+          // delete genre related to edited book
+          const { error: errorBooksGenres } = await supabase.from('book_books_genres').delete().eq('book_id', body.id);
+          if (errorBooksGenres) {
+            res.status(422).json({ error: errorBooksGenres.message });
+          }
+          // if edited book have genre
+          if (body.genre?.length > 0) {
+            // create array of genre of a edited book
+            let genre = [];
+            body.genre.forEach((item: any) => {
+              genre.push({
+                book_id: body.id,
+                genre_id: item.value,
+              });
             });
-          });
-          // insert genre of a edited book to book_books_genres table
-          const { error } = await supabase.from('book_books_genres').insert(genre);
-          if (error) {
+            // insert genre of a edited book to book_books_genres table
+            const { error } = await supabase.from('book_books_genres').insert(genre);
+            if (error) {
+              res.status(422).json({ error: error.message });
+            }
+          }
+          // Write logs
+          const errorLogs = await writeLogs(sessionPut.user_id, 'update', 'book_books', body.id);
+          if (errorLogs) {
             res.status(422).json({ error: error.message });
           }
+          res.status(201).json({ message: 'Success update book' });
         }
-        res.status(201).json({ message: 'Success update book' });
       }
       break;
 
     case 'DELETE':
-      if (!header) return res.status(401).json({ error: 'Please provide bearer token in headers' });
-      const { data: session } = await supabase.from('book_sessions').select('*').eq('token', token).single();
-      if (session) {
+      // Check session
+      const sessionDelete = await getSessionToken(res, header, token);
+      if (sessionDelete) {
         if (!query.id) {
           res.status(422).json({ error: 'Id required' });
         } else {
@@ -169,10 +187,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           if (error || errorBooksGenres) {
             res.status(422).json({ error: error.message });
           }
+          // Write logs
+          const errorLogs = await writeLogs(sessionDelete.user_id, 'delete', 'book_books', query.id);
+          if (errorLogs) {
+            res.status(422).json({ error: error.message });
+          }
           res.status(200).json({ message: 'Success delete book' });
         }
-      } else {
-        res.status(401).json({ message: 'Token invalid' });
       }
       break;
 
