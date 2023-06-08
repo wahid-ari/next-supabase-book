f116c398546eaaf60ba7acc495e6d52b277189e5
wahid-ari <wahiid.arii@gmail.com>
2023-06-08 19:56:47 +0700

    update quote api endpoint, secure create, update, delete

--- pages/api/quote/index.ts
+++ pages/api/quote/index.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { supabase } from '@libs/supabase';
+import { supabase, getSessionToken, writeLogs } from '@libs/supabase';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method, body, query } = req;
@@ -45,102 +45,120 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       break;
 
     case 'POST':
-      if (!body.quote) {
-        res.status(422).json({ error: 'Quote required' });
-      } else {
-        // get tags string from array
-        let tags_string = ',';
-        body.tags?.forEach((item: any) => {
-          tags_string = tags_string + ', ' + item.label;
-        });
-        let clean_tags_string = tags_string.replace(',,', '').replace(' ', '');
-        // insert quote
-        const { data, error } = await supabase
-          .from('book_quotes')
-          .insert([
-            {
-              author_id: body.author_id,
-              quote: body.quote,
-              tags: clean_tags_string == ',' ? '' : clean_tags_string,
-            },
-          ])
-          .select();
-        if (error) {
-          res.status(422).json({ error: error.message });
-        }
-        // get quote id after inserting
-        const quoteId = data[0].id;
-        // if new quote have tags
-        if (body.tags?.length > 0) {
-          // create array of tags of a quote
-          let tags = [];
-          body.tags.forEach((item: any) => {
-            tags.push({
-              quote_id: quoteId,
-              tag_id: item.value,
-            });
+      // Check session
+      const sessionPost = await getSessionToken(res, header, token);
+      if (sessionPost) {
+        if (!body.quote) {
+          res.status(422).json({ error: 'Quote required' });
+        } else {
+          // get tags string from array
+          let tags_string = ',';
+          body.tags?.forEach((item: any) => {
+            tags_string = tags_string + ', ' + item.label;
           });
-          // insert tags of a quote to book_quotes_tags table
-          const { error } = await supabase.from('book_quotes_tags').insert(tags);
+          let clean_tags_string = tags_string.replace(',,', '').replace(' ', '');
+          // insert quote
+          const { data, error } = await supabase
+            .from('book_quotes')
+            .insert([
+              {
+                author_id: body.author_id,
+                quote: body.quote,
+                tags: clean_tags_string == ',' ? '' : clean_tags_string,
+              },
+            ])
+            .select();
           if (error) {
             res.status(422).json({ error: error.message });
           }
+          // get quote id after inserting
+          const quoteId = data[0].id;
+          // if new quote have tags
+          if (body.tags?.length > 0) {
+            // create array of tags of a quote
+            let tags = [];
+            body.tags.forEach((item: any) => {
+              tags.push({
+                quote_id: quoteId,
+                tag_id: item.value,
+              });
+            });
+            // insert tags of a quote to book_quotes_tags table
+            const { error } = await supabase.from('book_quotes_tags').insert(tags);
+            if (error) {
+              res.status(422).json({ error: error.message });
+            }
+          }
+          // Write logs
+          const errorLogs = await writeLogs(sessionPost.user_id, 'create', 'book_quotes');
+          if (errorLogs) {
+            res.status(422).json({ error: error.message });
+          }
+          res.status(200).json({ message: 'Success add quote' });
         }
-        res.status(200).json({ message: 'Success add quote' });
       }
       break;
 
     case 'PUT':
-      if (!body.quote) {
-        res.status(422).json({ error: 'Quote required' });
-      } else {
-        // get tags string from array
-        let tags_string = ',';
-        body.tags.forEach((item: any) => {
-          tags_string = tags_string + ', ' + item.label;
-        });
-        let clean_tags_string = tags_string.replace(',,', '').replace(' ', '');
-        // update quote
-        const { error } = await supabase
-          .from('book_quotes')
-          .update({
-            author_id: body.author_id,
-            quote: body.quote,
-            tags: clean_tags_string,
-          })
-          .eq('id', body.id);
-        if (error) {
-          res.status(422).json({ error: error.message });
-        }
-        // delete tags related to edited quote
-        const { error: errorQuotesTags } = await supabase.from('book_quotes_tags').delete().eq('quote_id', body.id);
-        if (errorQuotesTags) {
-          res.status(422).json({ error: errorQuotesTags.message });
-        }
-        // if edited quote have tags
-        if (body.tags?.length > 0) {
-          // create array of tags of a edited quote
-          let tags = [];
+      // Check session
+      const sessionPut = await getSessionToken(res, header, token);
+      if (sessionPut) {
+        if (!body.quote) {
+          res.status(422).json({ error: 'Quote required' });
+        } else {
+          // get tags string from array
+          let tags_string = ',';
           body.tags.forEach((item: any) => {
-            tags.push({
-              quote_id: body.id,
-              tag_id: item.value,
-            });
+            tags_string = tags_string + ', ' + item.label;
           });
-          // insert tags of a edited quote to book_quotes_tags table
-          const { error } = await supabase.from('book_quotes_tags').insert(tags);
+          let clean_tags_string = tags_string.replace(',,', '').replace(' ', '');
+          // update quote
+          const { error } = await supabase
+            .from('book_quotes')
+            .update({
+              author_id: body.author_id,
+              quote: body.quote,
+              tags: clean_tags_string,
+            })
+            .eq('id', body.id);
           if (error) {
             res.status(422).json({ error: error.message });
           }
+          // delete tags related to edited quote
+          const { error: errorQuotesTags } = await supabase.from('book_quotes_tags').delete().eq('quote_id', body.id);
+          if (errorQuotesTags) {
+            res.status(422).json({ error: errorQuotesTags.message });
+          }
+          // if edited quote have tags
+          if (body.tags?.length > 0) {
+            // create array of tags of a edited quote
+            let tags = [];
+            body.tags.forEach((item: any) => {
+              tags.push({
+                quote_id: body.id,
+                tag_id: item.value,
+              });
+            });
+            // insert tags of a edited quote to book_quotes_tags table
+            const { error } = await supabase.from('book_quotes_tags').insert(tags);
+            if (error) {
+              res.status(422).json({ error: error.message });
+            }
+          }
+          // Write logs
+          const errorLogs = await writeLogs(sessionPut.user_id, 'update', 'book_quotes', body.id);
+          if (errorLogs) {
+            res.status(422).json({ error: error.message });
+          }
+          res.status(201).json({ message: 'Success update quote' });
         }
-        res.status(201).json({ message: 'Success update quote' });
       }
       break;
 
     case 'DELETE':
-      if (!header) return res.status(401).json({ error: 'Please provide bearer token in headers' });
-      const { data: session } = await supabase.from('book_sessions').select('*').eq('token', token).single();
-      if (session) {
+      // Check session
+      const sessionDelete = await getSessionToken(res, header, token);
+      if (sessionDelete) {
         if (!query.id) {
           res.status(422).json({ error: 'Id required' });
         } else {
@@ -150,10 +168,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           if (error || errorQuotesTags) {
             res.status(422).json({ error: error.message });
           }
+          // Write logs
+          const errorLogs = await writeLogs(sessionDelete.user_id, 'delete', 'book_quotes', query.id);
+          if (errorLogs) {
+            res.status(422).json({ error: error.message });
+          }
           res.status(200).json({ message: 'Success delete quote' });
         }
-      } else {
-        res.status(401).json({ message: 'Token invalid' });
       }
       break;
 
