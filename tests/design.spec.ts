d38ec99bec507bc0acb1017f35ab9b53a9f50a8b
wahid-ari <wahiid.arii@gmail.com>
2023-06-11 13:25:51 +0700

    add test for heading, container, badge

--- tests/design.spec.ts
+++ tests/design.spec.ts
@@ -79,4 +79,176 @@ test.describe('Testing Design Page', () => {
     await expect(linkbutton).toHaveText('Link Button Tertary');
     await expect(linkbutton).toHaveAttribute('href', '/design#linkbutton');
   });
+
+  // Heading ----------------------------------------------------
+  test('renders a Heading H1 component', async ({ page }) => {
+    const heading = page.getByTestId('heading-h1');
+    await expect(heading).toBeVisible();
+    await expect(heading).toHaveClass(/mb-4 text-3xl font-medium text-neutral-800 dark:text-neutral-100/);
+    await expect(heading).toHaveText('Heading 1');
+  });
+  test('renders a Heading H2 component', async ({ page }) => {
+    const heading = page.getByTestId('heading-h2');
+    await expect(heading).toBeVisible();
+    await expect(heading).toHaveClass(/mb-4 text-2xl font-medium text-neutral-800 dark:text-neutral-100/);
+    await expect(heading).toHaveText('Heading 2');
+  });
+  test('renders a Heading H3 component', async ({ page }) => {
+    const heading = page.getByTestId('heading-h3');
+    await expect(heading).toBeVisible();
+    await expect(heading).toHaveClass(/mb-4 text-xl font-medium text-neutral-800 dark:text-neutral-100/);
+    await expect(heading).toHaveText('Heading 3');
+  });
+  test('renders a Heading H4 component', async ({ page }) => {
+    const heading = page.getByTestId('heading-h4');
+    await expect(heading).toBeVisible();
+    await expect(heading).toHaveClass(/mb-4 text-lg font-medium text-neutral-800 dark:text-neutral-100/);
+    await expect(heading).toHaveText(/Heading 4/);
+  });
+
+  // Container ----------------------------------------------------
+  test('renders a Container component', async ({ page }) => {
+    const container = page.getByTestId('container');
+    await expect(container).toBeVisible();
+    await expect(container).toHaveClass(/p-8 relative mb-2 rounded-md border bg-white/);
+    await expect(container).toHaveText('Container');
+  });
+  test('renders a Container Small component', async ({ page }) => {
+    const container = page.getByTestId('container-small');
+    await expect(container).toBeVisible();
+    await expect(container).toHaveClass(/p-2 relative mb-2 rounded-md border bg-white/);
+    await expect(container).toHaveText('Container Small');
+  });
+
+  // Badge ----------------------------------------------------
+  test('renders a Badge component', async ({ page }) => {
+    const badge = page.getByTestId('badge');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-xs whitespace-nowrap bg-blue-100 font-semibold text-sky-600 dark:bg-sky-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('badge');
+  });
+  test('renders a Badge Dark component', async ({ page }) => {
+    const badge = page.getByTestId('badge-dark');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-xs whitespace-nowrap bg-gray-100 font-semibold text-gray-600 dark:bg-gray-600 dark:bg-opacity-10 dark:text-gray-400/
+    );
+    await expect(badge).toHaveText('dark');
+  });
+  test('renders a Badge Red component', async ({ page }) => {
+    const badge = page.getByTestId('badge-red');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-xs whitespace-nowrap bg-red-100 font-semibold text-red-600 dark:bg-red-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('red');
+  });
+  test('renders a Badge Green component', async ({ page }) => {
+    const badge = page.getByTestId('badge-green');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-xs whitespace-nowrap bg-green-100 font-semibold text-green-600 dark:bg-green-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('green');
+  });
+  test('renders a Badge Yellow component', async ({ page }) => {
+    const badge = page.getByTestId('badge-yellow');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-xs whitespace-nowrap bg-yellow-100 font-semibold text-yellow-600 dark:bg-yellow-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('yellow');
+  });
+  test('renders a Badge Indigo component', async ({ page }) => {
+    const badge = page.getByTestId('badge-indigo');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-xs whitespace-nowrap bg-indigo-100 font-semibold text-indigo-600 dark:bg-indigo-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('indigo');
+  });
+  test('renders a Badge Purple component', async ({ page }) => {
+    const badge = page.getByTestId('badge-purple');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-xs whitespace-nowrap bg-purple-100 font-semibold text-purple-600 dark:bg-purple-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('purple');
+  });
+  test('renders a Badge Pink component', async ({ page }) => {
+    const badge = page.getByTestId('badge-pink');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-xs whitespace-nowrap bg-pink-100 font-semibold text-pink-600 dark:bg-pink-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('pink');
+  });
+
+  // Badge Large ----------------------------------------------------
+  test('renders a Badge Large component', async ({ page }) => {
+    const badge = page.getByTestId('badge-large');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-sm whitespace-nowrap bg-blue-100 font-semibold text-sky-600 dark:bg-sky-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('badge');
+  });
+  test('renders a Badge Dark Large component', async ({ page }) => {
+    const badge = page.getByTestId('badge-dark-large');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-sm whitespace-nowrap bg-gray-100 font-semibold text-gray-600 dark:bg-gray-600 dark:bg-opacity-10 dark:text-gray-400/
+    );
+    await expect(badge).toHaveText('dark');
+  });
+  test('renders a Badge Red Large component', async ({ page }) => {
+    const badge = page.getByTestId('badge-red-large');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-sm whitespace-nowrap bg-red-100 font-semibold text-red-600 dark:bg-red-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('red');
+  });
+  test('renders a Badge Green Large component', async ({ page }) => {
+    const badge = page.getByTestId('badge-green-large');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-sm whitespace-nowrap bg-green-100 font-semibold text-green-600 dark:bg-green-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('green');
+  });
+  test('renders a Badge Yellow Large component', async ({ page }) => {
+    const badge = page.getByTestId('badge-yellow-large');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-sm whitespace-nowrap bg-yellow-100 font-semibold text-yellow-600 dark:bg-yellow-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('yellow');
+  });
+  test('renders a Badge Indigo Large component', async ({ page }) => {
+    const badge = page.getByTestId('badge-indigo-large');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-sm whitespace-nowrap bg-indigo-100 font-semibold text-indigo-600 dark:bg-indigo-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('indigo');
+  });
+  test('renders a Badge Purple Large component', async ({ page }) => {
+    const badge = page.getByTestId('badge-purple-large');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-sm whitespace-nowrap bg-purple-100 font-semibold text-purple-600 dark:bg-purple-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('purple');
+  });
+  test('renders a Badge Pink Large component', async ({ page }) => {
+    const badge = page.getByTestId('badge-pink-large');
+    await expect(badge).toBeVisible();
+    await expect(badge).toHaveClass(
+      /text-sm whitespace-nowrap bg-pink-100 font-semibold text-pink-600 dark:bg-pink-600 dark:bg-opacity-10/
+    );
+    await expect(badge).toHaveText('pink');
+  });
 });
