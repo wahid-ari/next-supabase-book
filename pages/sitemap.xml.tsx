064def2361a5398822ca820a93c6892ca6ed2f7b
wahid-ari <wahiid.arii@gmail.com>
2023-06-20 22:23:05 +0700

    add session page

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -4,6 +4,8 @@ function generateSiteMap(books: any, authors: any, genres: any, tags: any) {
   const timeElapsed = Date.now();
   const today = new Date(timeElapsed);
 
+  // TODO create endpoint for sitemap
+
   return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
       
