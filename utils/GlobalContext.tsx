657fd0d4c8aa3ff9478aa0f3d046b4f7552517cb
wahid-ari <wahiid.arii@gmail.com>
2023-06-21 13:33:35 +0700

    update tsconfig

--- utils/GlobalContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext, ReactNode, useState } from 'react';
-
-export const GlobalContext = createContext(null);
-
-export const GlobalProvider = ({ children }: { children: ReactNode }) => {
-  const [showNav, setShowNav] = useState(false);
-
-  return <GlobalContext.Provider value={{ showNav, setShowNav }}>{children}</GlobalContext.Provider>;
-};
