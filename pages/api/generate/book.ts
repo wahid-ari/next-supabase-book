3afcf808431ac19a941fd27afb1b1d5bb869f2e3
wahid-ari <wahiid.arii@gmail.com>
2023-06-28 20:35:44 +0700

    add slug column in author, book, genre and tag table

--- pages/api/generate/book.ts
+++ pages/api/generate/book.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { supabase } from '@libs/supabase';
 import books from '@data/books.json' assert { type: 'json' };
+import slug from 'slug';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method, query } = req;
@@ -11,11 +12,34 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   switch (method) {
     case 'GET':
-      if (!query.generate) {
+      if (!query.generate && !query.slug) {
         const { data } = await supabase.from('book_books').select(`*`).order('id');
         res.status(200).send(JSON.stringify(data, null, 2));
       } else if (query.generate == 'true') {
         const { data, error } = await supabase.from('book_books').insert(books);
+        res.status(200).json(data);
+      } else if (query.slug == 'true') {
+        const { data } = await supabase.from('book_books').select(`*`).order('id');
+        for (const row of data) {
+          // console.log(row.title);
+          let splitTitle = row.title?.split('(')[0];
+          // console.log(splitTitle);
+          let sliceTitle = splitTitle.slice(0, 80);
+          let nameSlug = slug(sliceTitle);
+          const { data: isSlugExist } = await supabase.from('book_books').select(`*`).eq('slug', nameSlug).order('id');
+          // if slug already exist, add id to slug to make it unique
+          if (isSlugExist.length > 0) {
+            nameSlug = `${nameSlug}-${row.id}`;
+          }
+          console.log(nameSlug);
+          // console.log('----------------------------------------------------\n');
+          const { error } = await supabase
+            .from('book_books')
+            .update({
+              slug: nameSlug,
+            })
+            .eq('id', row.id);
+        }
         res.status(200).json(data);
       } else {
         const { data } = await supabase
--- pages/api/generate/genre.ts
+++ pages/api/generate/genre.ts
@@ -1,17 +1,36 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { supabase } from '@libs/supabase';
 import genres from '@data/genres.json' assert { type: 'json' };
+import slug from 'slug';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method, query } = req;
 
   switch (method) {
     case 'GET':
-      if (!query.generate) {
+      if (!query.generate && !query.slug) {
         const { data } = await supabase.from('book_genres').select(`*`).order('id');
         res.status(200).send(JSON.stringify(data, null, 2));
       } else if (query.generate == 'true') {
         const { data } = await supabase.from('book_genres').insert(genres);
+        res.status(200).json(data);
+      } else if (query.slug == 'true') {
+        const { data } = await supabase.from('book_genres').select(`*`).order('id');
+        for (const row of data) {
+          let nameSlug = slug(row.name);
+          const { data: isSlugExist } = await supabase.from('book_genres').select(`*`).eq('slug', nameSlug).order('id');
+          // if slug already exist, add id to slug to make it unique
+          if (isSlugExist.length > 0) {
+            nameSlug = `${nameSlug}-${row.id}`;
+          }
+          console.log(nameSlug);
+          const { error } = await supabase
+            .from('book_genres')
+            .update({
+              slug: nameSlug,
+            })
+            .eq('id', row.id);
+        }
         res.status(200).json(data);
       } else {
         const { data } = await supabase
