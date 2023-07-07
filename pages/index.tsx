5c9dffb930ed4db3d13c75a6a2da8de4e5717b81
wahid-ari <wahiid.arii@gmail.com>
2023-07-07 14:27:11 +0700

    update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ export default function Home() {
             <Link
               href='#section-books'
               className={clsx(
-                'group inline-flex items-center gap-2 rounded border border-orange-500 px-6 py-2 font-semibold transition-all',
+                'group inline-flex items-center gap-2 rounded-lg border border-orange-500 px-6 py-2 font-semibold transition-all',
                 'hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-800',
                 'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange-500'
               )}
@@ -238,13 +238,10 @@ export default function Home() {
             </Link>
           </div>
         </div>
-        <div className='mt-4'>
+        <div className='mt-4 divide-y dark:divide-neutral-800'>
           {sliceQuotes ? (
-            sliceQuotes.map((item: any, index: number) => (
-              <div
-                key={item.id}
-                className={clsx('mb-4 pb-4', index != sliceQuotes.length - 1 && 'border-b dark:border-b-neutral-800')}
-              >
+            sliceQuotes.map((item: any) => (
+              <div key={item.id} className='py-4'>
                 <p className='mb-1 text-justify text-base'>&#8220;{item.quote}&#8221;</p>
                 <Link
                   href={`authors/${item?.book_authors?.slug}`}
